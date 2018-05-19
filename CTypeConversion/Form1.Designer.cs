6b6dd7d4de2fefa4df7c58a8a528e7393e077329
Jet Sun <18820095105@139.com>
2018-05-19 20:49:25 +0800

    Add project files.

---
+++ CTypeConversion/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace CTypeConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ButTestAlias = new System.Windows.Forms.Button();
+            this.BtnAutoConversion = new System.Windows.Forms.Button();
+            this.BtnCharConvesrsion = new System.Windows.Forms.Button();
+            this.BtnTypeConversion = new System.Windows.Forms.Button();
+            this.BtnStringConversion = new System.Windows.Forms.Button();
+            this.BtnStringToCharArray = new System.Windows.Forms.Button();
+            this.BtnStringToByteArray = new System.Windows.Forms.Button();
+            this.BtnToBitArray = new System.Windows.Forms.Button();
+            this.BtnToHex = new System.Windows.Forms.Button();
+            this.DateTimeToLongInt = new System.Windows.Forms.Button();
+            this.BtnDateTimeToString = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(643, 248);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ButTestAlias
+            // 
+            this.ButTestAlias.Location = new System.Drawing.Point(708, 12);
+            this.ButTestAlias.Name = "ButTestAlias";
+            this.ButTestAlias.Size = new System.Drawing.Size(130, 38);
+            this.ButTestAlias.TabIndex = 2;
+            this.ButTestAlias.Text = "TestAlias";
+            this.ButTestAlias.UseVisualStyleBackColor = true;
+            this.ButTestAlias.Click += new System.EventHandler(this.ButTestAlias_Click);
+            // 
+            // BtnAutoConversion
+            // 
+            this.BtnAutoConversion.Location = new System.Drawing.Point(708, 72);
+            this.BtnAutoConversion.Name = "BtnAutoConversion";
+            this.BtnAutoConversion.Size = new System.Drawing.Size(130, 38);
+            this.BtnAutoConversion.TabIndex = 3;
+            this.BtnAutoConversion.Text = "Test隐式转换 ";
+            this.BtnAutoConversion.UseVisualStyleBackColor = true;
+            this.BtnAutoConversion.Click += new System.EventHandler(this.BtnAutoConversion_Click);
+            // 
+            // BtnCharConvesrsion
+            // 
+            this.BtnCharConvesrsion.Location = new System.Drawing.Point(708, 185);
+            this.BtnCharConvesrsion.Name = "BtnCharConvesrsion";
+            this.BtnCharConvesrsion.Size = new System.Drawing.Size(130, 38);
+            this.BtnCharConvesrsion.TabIndex = 4;
+            this.BtnCharConvesrsion.Text = "Test字符转换";
+            this.BtnCharConvesrsion.UseVisualStyleBackColor = true;
+            this.BtnCharConvesrsion.Click += new System.EventHandler(this.BtnCharConvesrsion_Click);
+            // 
+            // BtnTypeConversion
+            // 
+            this.BtnTypeConversion.Location = new System.Drawing.Point(708, 127);
+            this.BtnTypeConversion.Name = "BtnTypeConversion";
+            this.BtnTypeConversion.Size = new System.Drawing.Size(130, 38);
+            this.BtnTypeConversion.TabIndex = 5;
+            this.BtnTypeConversion.Text = "Test强制转换";
+            this.BtnTypeConversion.UseVisualStyleBackColor = true;
+            this.BtnTypeConversion.Click += new System.EventHandler(this.BtnTypeConversion_Click);
+            // 
+            // BtnStringConversion
+            // 
+            this.BtnStringConversion.Location = new System.Drawing.Point(686, 247);
+            this.BtnStringConversion.Name = "BtnStringConversion";
+            this.BtnStringConversion.Size = new System.Drawing.Size(193, 38);
+            this.BtnStringConversion.TabIndex = 6;
+            this.BtnStringConversion.Text = "字符串转换数字";
+            this.BtnStringConversion.UseVisualStyleBackColor = true;
+            this.BtnStringConversion.Click += new System.EventHandler(this.BtnStringConversion_Click);
+            // 
+            // BtnStringToCharArray
+            // 
+            this.BtnStringToCharArray.Location = new System.Drawing.Point(686, 306);
+            this.BtnStringToCharArray.Name = "BtnStringToCharArray";
+            this.BtnStringToCharArray.Size = new System.Drawing.Size(193, 38);
+            this.BtnStringToCharArray.TabIndex = 7;
+            this.BtnStringToCharArray.Text = "字符串转字符数组";
+            this.BtnStringToCharArray.UseVisualStyleBackColor = true;
+            this.BtnStringToCharArray.Click += new System.EventHandler(this.BtnStringToCharArray_Click);
+            // 
+            // BtnStringToByteArray
+            // 
+            this.BtnStringToByteArray.Location = new System.Drawing.Point(686, 362);
+            this.BtnStringToByteArray.Name = "BtnStringToByteArray";
+            this.BtnStringToByteArray.Size = new System.Drawing.Size(193, 38);
+            this.BtnStringToByteArray.TabIndex = 8;
+            this.BtnStringToByteArray.Text = "字符串转字节数组";
+            this.BtnStringToByteArray.UseVisualStyleBackColor = true;
+            this.BtnStringToByteArray.Click += new System.EventHandler(this.BtnStringToByteArray_Click);
+            // 
+            // BtnToBitArray
+            // 
+            this.BtnToBitArray.Location = new System.Drawing.Point(686, 421);
+            this.BtnToBitArray.Name = "BtnToBitArray";
+            this.BtnToBitArray.Size = new System.Drawing.Size(193, 38);
+            this.BtnToBitArray.TabIndex = 9;
+            this.BtnToBitArray.Text = "数值转字节数组";
+            this.BtnToBitArray.UseVisualStyleBackColor = true;
+            this.BtnToBitArray.Click += new System.EventHandler(this.BtnToBitArray_Click);
+            // 
+            // BtnToHex
+            // 
+            this.BtnToHex.Location = new System.Drawing.Point(686, 478);
+            this.BtnToHex.Name = "BtnToHex";
+            this.BtnToHex.Size = new System.Drawing.Size(193, 38);
+            this.BtnToHex.TabIndex = 10;
+            this.BtnToHex.Text = "数值转16进制数";
+            this.BtnToHex.UseVisualStyleBackColor = true;
+            this.BtnToHex.Click += new System.EventHandler(this.BtnToHex_Click);
+            // 
+            // DateTimeToLongInt
+            // 
+            this.DateTimeToLongInt.Location = new System.Drawing.Point(686, 536);
+            this.DateTimeToLongInt.Name = "DateTimeToLongInt";
+            this.DateTimeToLongInt.Size = new System.Drawing.Size(193, 38);
+            this.DateTimeToLongInt.TabIndex = 11;
+            this.DateTimeToLongInt.Text = "日期转长整形数据";
+            this.DateTimeToLongInt.UseVisualStyleBackColor = true;
+            this.DateTimeToLongInt.Click += new System.EventHandler(this.DateTimeToLongInt_Click);
+            // 
+            // BtnDateTimeToString
+            // 
+            this.BtnDateTimeToString.Location = new System.Drawing.Point(686, 592);
+            this.BtnDateTimeToString.Name = "BtnDateTimeToString";
+            this.BtnDateTimeToString.Size = new System.Drawing.Size(193, 38);
+            this.BtnDateTimeToString.TabIndex = 12;
+            this.BtnDateTimeToString.Text = "日期转字符串";
+            this.BtnDateTimeToString.UseVisualStyleBackColor = true;
+            this.BtnDateTimeToString.Click += new System.EventHandler(this.BtnDateTimeToString_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 665);
+            this.Controls.Add(this.BtnDateTimeToString);
+            this.Controls.Add(this.DateTimeToLongInt);
+            this.Controls.Add(this.BtnToHex);
+            this.Controls.Add(this.BtnToBitArray);
+            this.Controls.Add(this.BtnStringToByteArray);
+            this.Controls.Add(this.BtnStringToCharArray);
+            this.Controls.Add(this.BtnStringConversion);
+            this.Controls.Add(this.BtnTypeConversion);
+            this.Controls.Add(this.BtnCharConvesrsion);
+            this.Controls.Add(this.BtnAutoConversion);
+            this.Controls.Add(this.ButTestAlias);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ButTestAlias;
+        private System.Windows.Forms.Button BtnAutoConversion;
+        private System.Windows.Forms.Button BtnCharConvesrsion;
+        private System.Windows.Forms.Button BtnTypeConversion;
+        private System.Windows.Forms.Button BtnStringConversion;
+        private System.Windows.Forms.Button BtnStringToCharArray;
+        private System.Windows.Forms.Button BtnStringToByteArray;
+        private System.Windows.Forms.Button BtnToBitArray;
+        private System.Windows.Forms.Button BtnToHex;
+        private System.Windows.Forms.Button DateTimeToLongInt;
+        private System.Windows.Forms.Button BtnDateTimeToString;
+    }
+}
+
---
+++ CTypeConversion/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTypeConversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TestAlias()
+        {
+            // this.textBox1 是一个文本框，类型为 System.Windows.Forms.TextBox
+            // 设计中已经将其 Multiline 属性设置为 true
+            byte a = 1; char b = 'a'; short c = 1;
+            int d = 2; long e = 3; uint f = 4; bool g = true;
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("byte -> " + a.GetType().FullName + "\n");
+            this.textBox1.AppendText("char -> " + b.GetType().FullName + "\n");
+            this.textBox1.AppendText("short -> " + c.GetType().FullName + "\n");
+            this.textBox1.AppendText("int -> " + d.GetType().FullName + "\n");
+            this.textBox1.AppendText("long -> " + e.GetType().FullName + "\n");
+            this.textBox1.AppendText("uint -> " + f.GetType().FullName + "\n");
+            this.textBox1.AppendText("bool -> " + g.GetType().FullName + "\n");
+        }
+
+        private void TestAutoConversion()
+        {
+            byte a = 1; short b = a; int c = b;
+            long d = c; float e = d; double f = e;
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("byte a = " + a.ToString("F") + "\n");
+            this.textBox1.AppendText("short b = " + b.ToString("F") + "\n");
+            this.textBox1.AppendText("int c = " + c.ToString("F") + "\n");
+            this.textBox1.AppendText("long d = " + d.ToString("F") + "\n");
+            this.textBox1.AppendText("float e = " + e.ToString("F") + "\n");
+            this.textBox1.AppendText("double f = " + f.ToString("F") + "\n");
+        }
+
+        private void TestChar()
+        {
+            char ch = 'a'; short ii = 65;
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("The ASCII code of /'" + ch + "/' is: " + (short)ch + "/n");
+            this.textBox1.AppendText("ASCII is " + ii.ToString() + ", the char is: " + (char)ii + "/n");
+            char cn = '中'; short uc = 22478;
+            this.textBox1.AppendText("The Unicode of /'" + cn + "/' is: " + (short)cn + "/n");
+            this.textBox1.AppendText("Unicode is " + uc.ToString() + ", the char is: " + (char)uc + "/n");
+        }
+
+        private void ButTestAlias_Click(object sender, EventArgs e)
+        {
+            TestAlias();
+
+        }
+
+        private void BtnAutoConversion_Click(object sender, EventArgs e)
+        {
+            TestAutoConversion();
+        }
+
+        private void BtnTypeConversion_Click(object sender, EventArgs e)
+        {
+            int g = 1;
+            //short h = g;// Implicitly convert wrong；
+            short h = (short)g;
+            this.textBox1.AppendText("h = " + h.ToString() + "\n");
+
+            short i = 256; //265 = 255 + 10,shor max 255;
+            byte j = (byte)i;
+            this.textBox1.AppendText("j("+i.ToString()+") = " + j.ToString() + "\n");
+
+        }
+
+        private void BtnCharConvesrsion_Click(object sender, EventArgs e)
+        {
+            char ch = 'a';
+            short ii = 65;
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("The ASCII code of /'" + ch + "/' is: " + (short)ch + "\n");
+            this.textBox1.AppendText("ASCII is " + ii.ToString() + ", the char is: " + (char)ii + "\n");
+
+            char cn = '中';
+            short uc = 22269;
+            this.textBox1.AppendText("The Unicode of /'" + cn + "/' is: " + (short)cn + "\n");
+            this.textBox1.AppendText("Unicode is " + uc.ToString() + ", the char is: " + (char)uc + "\n");
+        }
+
+        private void BtnStringConversion_Click(object sender, EventArgs e)
+        {
+            float f = 54.321F;
+            string str = "123";
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("f = " + f.ToString() + "\n");
+            if (int.Parse(str) == 123)
+            {
+                this.textBox1.AppendText("str\"123\" convert to int successfully.");
+            }
+            else
+            {
+                this.textBox1.AppendText("str convert to int failed.");
+            }
+        }
+
+        private void BtnStringToCharArray_Click(object sender, EventArgs e)
+        {
+            //string to char[]
+            string str = "mytest";
+            char[] chars = str.ToCharArray();
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("Length of \"mytest \" is " + str.Length + "\n");
+            this.textBox1.AppendText("Length of char array is " + chars.Length + "\n");
+            this.textBox1.AppendText("char[0] = " + chars[0] + "\n");
+            this.textBox1.AppendText("char[1] = " + chars[1] + "\n");
+            this.textBox1.AppendText("char[2] = " + chars[2] + "\n");
+
+            //char[] to string
+            char[] tcs = { 't', 'e', 's', 't', ' ', 'm', 'e' };
+            string tstr = new string(tcs);
+            this.textBox1.AppendText("char array to string is " + tstr + "\n");
+            this.textBox1.AppendText("char array to string is " + tcs.ToString() + "\n");
+            string tstr1 = tcs.ToString();
+            this.textBox1.AppendText("char array to string is " + tstr1 + "\n");
+            char ch = tstr[3];
+            this.textBox1.AppendText("\"" + tstr + " \"[3] = " + ch.ToString());
+
+        }
+
+        private void BtnStringToByteArray_Click(object sender, EventArgs e)
+        {
+            //string to byte[]
+            string s = "C#语言";
+            byte[] b1 = System.Text.Encoding.Default.GetBytes(s);
+            byte[] b2 = System.Text.Encoding.Unicode.GetBytes(s);
+            string t1 = "", t2 = "";
+            foreach (byte b in b1)
+            {
+                t1 += b.ToString("") + " ";
+            }
+            foreach (byte b in b2)
+            {
+                t2 += b.ToString("") + " ";
+            }
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("b1.Length = " + b1.Length + "\n");
+            this.textBox1.AppendText(t1 + "\n");
+            this.textBox1.AppendText("b2.Length = " + b2.Length + "\n");
+            this.textBox1.AppendText(t2 + "\n");
+
+            //byte[] to string
+            byte[] bs = { 97, 98, 99, 100, 101, 102 };
+            string ss = System.Text.Encoding.ASCII.GetString(bs);
+            this.textBox1.AppendText("The string is: " + ss + "\n");
+        }
+
+        private void BtnToBitArray_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "在第 1 条中我们可以查到各种数值型需要使用多少字节的空间来保存数据。将某种数值类型的数据转换成字节数组的时候，得到的一定是相应大小的字节数组；同样，需要把字节数组转换成数值类型，也需要这个字节数组大于相应数值类型的字节数。\n" +
+                "现在介绍此类转换的主角：System.BitConverter。该类提供了 byte[] GetBytes(...) 方法将各种数值类型转换成字节数组，也提供了 ToInt32、ToInt16、ToInt64、ToUInt32、ToSignle、ToBoolean 等方法将字节数组转换成相应的数值类型。\n" +
+                "https://blog.csdn.net/gasx/article/details/638717";
+
+         }
+
+        private void BtnToHex_Click(object sender, EventArgs e)
+        {
+            //Int to Hex String
+            int a = 188;
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("a(10) = " + a.ToString() + "\n");
+            this.textBox1.AppendText("a(16) = " + a.ToString("x4") + "\n");
+            this.textBox1.AppendText("a(16) = " + a.ToString("X4") + "\n");
+
+            //HexString to Int
+            int b = int.Parse("AB", System.Globalization.NumberStyles.HexNumber);
+            int c = int.Parse("20", System.Globalization.NumberStyles.HexNumber);
+            this.textBox1.AppendText("AB(Hex) = " + b.ToString() + "\n");
+            this.textBox1.AppendText("20(Hex) = " + c.ToString() + "\n");
+        }
+
+        private void DateTimeToLongInt_Click(object sender, EventArgs e)
+        {
+            //DateTime  to longint
+            long longDate = DateTime.Now.Ticks;
+            DateTime DateFromLong = new DateTime(longDate);
+            this.textBox1.Text = "";
+            this.textBox1.AppendText("Long value of now: " + longDate.ToString() + "\n");
+            this.textBox1.AppendText("DateTime from long value: " + DateFromLong.ToString() + "\n");
+
+            // DateTime to double like VB6
+            double doubleDate = DateTime.Now.ToOADate();
+            DateTime theDate = DateTime.FromOADate(doubleDate);
+            this.textBox1.AppendText("Double value of now: " + doubleDate.ToString() + "\n");
+            this.textBox1.AppendText("DateTime from double value: " + theDate.ToString() + "\n");
+        }
+
+        private void BtnDateTimeToString_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string format;
+            this.textBox1.Text = "";
+            format = "yyyy-MM-dd HH:mm:ss";
+            this.textBox1.AppendText(format + ": " + now.ToString(format) + "\n");
+            format = "yy年M日d日";
+            this.textBox1.AppendText(format + ": " + now.ToString(format) + "\n");
+        }
+    }
+}
